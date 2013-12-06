8e9f42435e740bc6124ea2b821f44b616e368a38
mataddy <mataddy@outlook.com>
2013-12-06 17:03:28 -0600

    added tfidf

--- R/pls.R
+++ R/pls.R
@@ -1,31 +1,3 @@
-## Tools for manipulation of text count matrices ##
-stm2dg <- function(x, ...){
-  sparseMatrix(i=x$i, j=x$j, x=x$v,
-        dims=dim(x),dimnames=dimnames(x), ...) }
-
-## correlation 
-corr <- function(x, y){
-  if(inherits(x, "simple_triplet_matrix")) x <- stm2dg(x)
-  if(!inherits(x, "dgCMatrix")){ return(cor(x,y) ) }
-
-  n <- nrow(x)
-  v <- t(scale(y))
-  
-  r <- tcrossprod(t(x)/sdev(x), v)/(n-1)
-  dimnames(r) <- list(dimnames(x)[[2]], dimnames(y)[[2]])
-  return( r ) 
-}
-  
-## column standard deviation 
-sdev <- function(x){
-  if(is.null(dim(x))) return(sd(x))
-  if(inherits(x, "simple_triplet_matrix")) x <- stm2dg(x)
-  if(!inherits(x, "dgCMatrix")){ return(apply(as.matrix(x),2,sd)) }
-  n <- nrow(x)
-  s <- sqrt(colSums(x^2)/(n-1) - colSums(x)^2/(n^2 - n)) 
-  names(s) <- colnames(s)
-  return(s)  }
-
 ## minimalist partial least squares
 pls <- function(x, y, K=1, scale=TRUE, verb=TRUE){
 
